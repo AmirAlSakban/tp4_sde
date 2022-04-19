8e14def5b4ca1e48e82355516bd6607aaea61521
AmirAlSakban <amir.al@stud.fils.upb.ro>
2022-04-19 09:53:17 +0300

    tp4_19.04.2022

---
+++ main.py
@@ -0,0 +1,26 @@
+
+def on_button_pressed_b():
+    basic.show_number(input.temperature())
+input.on_button_pressed(Button.B, on_button_pressed_b)
+def on_forever():
+    is_logo_pressed = True
+    if input.logo_is_pressed():
+        is_logo_pressed = False
+    if is_logo_pressed == True:
+        basic.show_icon(IconNames.SQUARE)
+    else:
+        basic.show_icon(IconNames.SMALL_SQUARE)
+def on_sound_loud():
+    basic.show_icon(IconNames.LINE)
+input.on_sound(DetectedSound.LOUD, on_sound_loud)
+        
+        
+            
+   
+   # basic.show_number(input.light_level())
+    
+#input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
+
+
+   
+basic.forever(on_forever)
